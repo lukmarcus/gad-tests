cfcc1c2f4c091f1da1578f8ebda686d1efef8590
Marek Szumny <lukmarcus@gmail.com>
2024-08-24 23:55:29 +0200

    test: login scenario

---
+++ tests/login.spec.ts
@@ -0,0 +1,26 @@
+import { LoginPage } from '../src/pages/login.page';
+import { WelcomePage } from '../src/pages/welcome.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify login', () => {
+  test(
+    'login with correct credentials',
+    { tag: '@GAD-R02-01' },
+    async ({ page }) => {
+      // Arange
+      const userEmail = 'Moses.Armstrong@Feest.ca';
+      const userPassword = 'test1';
+      const loginPage = new LoginPage(page);
+
+      // Act
+      await loginPage.goto();
+      await loginPage.login(userEmail, userPassword);
+
+      const welcomePage = new WelcomePage(page);
+      const title = await welcomePage.title();
+
+      // Assert
+      expect(title).toContain('Welcome');
+    },
+  );
+});
