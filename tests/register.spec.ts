634cbdf1c62f6a42931264a986c18d3e3e97f222
Marek Szumny <lukmarcus@gmail.com>
2024-08-28 07:29:05 +0200

    feat: added faker to tests

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,6 +1,7 @@
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { faker } from '@faker-js/faker';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
@@ -9,10 +10,13 @@ test.describe('Verify register', () => {
     { tag: ['@GAD-R03-01', '@GAD-R03-02', '@GAD-R03-03'] },
     async ({ page }) => {
       // Arrange
-      const userFirstName = 'Janina';
-      const userLastName = 'Nowak';
-      const userEmail = `jntest${new Date().getTime()}@test.test1`;
-      const userPassword = 'testtest234';
+      const userFirstName = faker.person.firstName();
+      const userLastName = faker.person.lastName();
+      const userEmail = faker.internet.email({
+        firstName: userFirstName,
+        lastName: userLastName,
+      });
+      const userPassword = faker.internet.password();
 
       const registerPage = new RegisterPage(page);
 
