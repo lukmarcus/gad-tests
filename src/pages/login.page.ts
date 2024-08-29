e7efd2b89658c18d7c8cebcc230b7aeb46bfdd03
Marek Szumny <lukmarcus@gmail.com>
2024-08-29 21:43:55 +0200

    wip: login refactor

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,3 +1,4 @@
+import { LoginUser } from '../models/user.model';
 import { BasePage } from './base.page';
 import { Page } from '@playwright/test';
 
@@ -16,6 +17,12 @@ export class LoginPage extends BasePage {
   async login(email: string, password: string): Promise<void> {
     await this.userEmailInput.fill(email);
     await this.userPasswordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async loginNew(loginUserData: LoginUser): Promise<void> {
+    await this.userEmailInput.fill(loginUserData.userEmail);
+    await this.userPasswordInput.fill(loginUserData.userPassword);
     await this.loginButton.click();
   }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,3 +1,4 @@
+import { LoginUser } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
 import { testUser1 } from '../src/test-data/user.data';
@@ -9,13 +10,16 @@ test.describe('Verify login', () => {
     { tag: '@GAD-R02-01' },
     async ({ page }) => {
       // Arrange
-      const userEmail = testUser1.userEmail;
-      const userPassword = testUser1.userPassword;
+      const loginUserData: LoginUser = {
+        userEmail: testUser1.userEmail,
+        userPassword: testUser1.userPassword,
+      };
+
       const loginPage = new LoginPage(page);
 
       // Act
       await loginPage.goto();
-      await loginPage.login(userEmail, userPassword);
+      await loginPage.loginNew(loginUserData);
 
       const welcomePage = new WelcomePage(page);
       const title = await welcomePage.title();
