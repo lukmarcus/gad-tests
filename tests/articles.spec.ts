3fc7c28ef4fc1658e4a13e60cb99f6fe999c7432
Marek Szumny <lukmarcus@gmail.com>
2024-09-03 14:41:10 +0200

    test: user can access article

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -24,21 +24,6 @@ test.describe('Verify articles', () => {
     await expect.soft(addArticleView.header).toBeVisible();
   });
 
-  test('create new article', { tag: '@GAD-R04-01' }, async ({ page }) => {
-    // Arrange
-    const articlePage = new ArticlePage(page);
-    const articleData = randomNewArticle();
-
-    // Act
-    await addArticleView.createArticle(articleData);
-
-    // Assert
-    await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
-    await expect
-      .soft(articlePage.articleBody)
-      .toHaveText(articleData.body, { useInnerText: true });
-  });
-
   test(
     'reject creating article without title',
     { tag: '@GAD-R04-01' },
