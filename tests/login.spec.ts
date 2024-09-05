2989a2d99acf42422398e765c3dbaae023e4333f
Marek Szumny <lukmarcus@gmail.com>
2024-09-05 16:13:53 +0200

    chore: refactor to aaa formatting

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,14 +10,14 @@ test.describe('Verify login', () => {
     { tag: '@GAD-R02-01' },
     async ({ page }) => {
       // Arrange
-      const loginPage = new LoginPage(page);
       const expectedLoginTitle = 'Welcome';
+      const loginPage = new LoginPage(page);
+      const welcomePage = new WelcomePage(page);
 
       // Act
       await loginPage.goto();
       await loginPage.login(testUser1);
 
-      const welcomePage = new WelcomePage(page);
       const title = await welcomePage.getTitle();
 
       // Assert
@@ -32,12 +32,12 @@ test.describe('Verify login', () => {
       // Arrange
       const expectedLoginTitle = 'Login';
       const expectedLoginErrorText = 'Invalid username or password';
+      const loginPage = new LoginPage(page);
+
       const loginUserData: LoginUserModel = {
         userEmail: testUser1.userEmail,
         userPassword: 'incorrectPassword',
       };
-
-      const loginPage = new LoginPage(page);
 
       // Act
       await loginPage.goto();
