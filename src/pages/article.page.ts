c9d84ead730cb5aaa08d8aa9b7a17ea452d5edf0
Marek Szumny <lukmarcus@gmail.com>
2024-09-08 00:47:10 +0200

    wip: comment lifecycle, verify comment

--- src/pages/article.page.ts
+++ src/pages/article.page.ts
@@ -1,6 +1,11 @@
 import { MainMenuComponent } from '../components/main-menu.component';
 import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
+
+interface ArticleComment {
+  body: Locator;
+  link: Locator;
+}
 
 export class ArticlePage extends BasePage {
   url = '/article.html';
@@ -21,5 +26,16 @@ export class ArticlePage extends BasePage {
       await dialog.accept();
     });
     await this.deleteIcon.click();
+  }
+
+  getArticleComment(body: string): ArticleComment {
+    const commentContainer = this.page
+      .locator('.comment-container')
+      .filter({ hasText: body });
+
+    return {
+      body: commentContainer.locator(':text("comment:") + span'),
+      link: commentContainer.locator("[id^='gotoComment']"),
+    };
   }
 }
