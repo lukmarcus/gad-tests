d3bb6e663024adbe84842f177f491f88053ca27e
Marek Szumny <lukmarcus@gmail.com>
2024-09-08 12:07:58 +0200

    wip: comment lifecycle, edit comment

--- tests/end-to-end/comment-lifecycle.spec.ts
+++ tests/end-to-end/comment-lifecycle.spec.ts
@@ -8,6 +8,7 @@ import { LoginPage } from '../../src/pages/login.page';
 import { testUser1 } from '../../src/test-data/user.data';
 import { AddArticleView } from '../../src/views/add-article.view';
 import { AddCommentView } from '../../src/views/add-comment.view';
+import { EditCommentView } from '../../src/views/edit-comment.view';
 import { expect, test } from '@playwright/test';
 
 test.describe('Create, verify and delete comment', () => {
@@ -18,6 +19,7 @@ test.describe('Create, verify and delete comment', () => {
   let articleData: AddArticleModel;
   let addCommentView: AddCommentView;
   let commentPage: CommentPage;
+  let editCommentView: EditCommentView;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
@@ -26,6 +28,7 @@ test.describe('Create, verify and delete comment', () => {
     articlePage = new ArticlePage(page);
     addCommentView = new AddCommentView(page);
     commentPage = new CommentPage(page);
+    editCommentView = new EditCommentView(page);
 
     articleData = prepareRandomArticle();
 
@@ -41,6 +44,7 @@ test.describe('Create, verify and delete comment', () => {
     // Arrange
     const expectedAddCommentHeader = 'Add New Comment';
     const expectedCommentCreatedPopup = 'Comment was created';
+    const expectedCommentUpdatePopup = 'Comment was updated';
 
     const newCommentData = prepareRandomComment();
 
@@ -50,8 +54,7 @@ test.describe('Create, verify and delete comment', () => {
       expectedAddCommentHeader,
     );
 
-    await addCommentView.bodyInput.fill(newCommentData.body);
-    await addCommentView.saveButton.click();
+    await addCommentView.createComment(newCommentData);
 
     // Assert
     await expect(articlePage.alertPopup).toHaveText(
@@ -66,5 +69,22 @@ test.describe('Create, verify and delete comment', () => {
 
     // Assert
     await expect(commentPage.commentBody).toHaveText(newCommentData.body);
+
+    // Edit comment
+    // Act
+    const editCommentData = prepareRandomComment();
+
+    await commentPage.editButton.click();
+    await editCommentView.updateComment(editCommentData);
+
+    // Assert
+    await expect(commentPage.alertPopup).toHaveText(expectedCommentUpdatePopup);
+    await expect(commentPage.commentBody).toHaveText(editCommentData.body);
+    await commentPage.returnLink.click();
+
+    const updatedArticleComment = articlePage.getArticleComment(
+      editCommentData.body,
+    );
+    await expect(updatedArticleComment.body).toHaveText(editCommentData.body);
   });
 });
