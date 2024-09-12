49cd19d2f7e1fd1fd6c4a8c9c7ef5f0271d079e9
Marek Szumny <lukmarcus@gmail.com>
2024-09-12 23:38:21 +0200

    chore: use page object methods in menu component

--- src/components/main-menu.component.ts
+++ src/components/main-menu.component.ts
@@ -1,9 +1,27 @@
+import { ArticlePage } from '@_src/pages/article.page';
+import { CommentsPage } from '@_src/pages/comments.page';
+import { HomePage } from '@_src/pages/home.page';
 import { Page } from '@playwright/test';
 
 export class MainMenuComponent {
-  homePage = this.page.getByRole('link', { name: '🦎 GAD' });
+  homePageLink = this.page.getByRole('link', { name: '🦎 GAD' });
   commentsButton = this.page.getByTestId('open-comments');
   articlesButton = this.page.getByTestId('open-articles');
 
   constructor(private page: Page) {}
+
+  async clickCommentsButton(): Promise<CommentsPage> {
+    await this.commentsButton.click();
+    return new CommentsPage(this.page);
+  }
+
+  async clickArticlesButton(): Promise<ArticlePage> {
+    await this.articlesButton.click();
+    return new ArticlePage(this.page);
+  }
+
+  async clickHomePageLink(): Promise<HomePage> {
+    await this.homePageLink.click();
+    return new HomePage(this.page);
+  }
 }
--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,6 +1,5 @@
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify main menu buttons', () => {
@@ -11,11 +10,10 @@ test.describe('Verify main menu buttons', () => {
       // Arrange
       const expectedCommentsTitle = 'Comments';
       const articlesPage = new ArticlesPage(page);
-      const commentsPage = new CommentsPage(page);
 
       // Act
       await articlesPage.goto();
-      await articlesPage.mainMenu.commentsButton.click();
+      const commentsPage = await articlesPage.mainMenu.clickCommentsButton();
       const title = await commentsPage.getTitle();
 
       // Assert
@@ -30,11 +28,10 @@ test.describe('Verify main menu buttons', () => {
       // Arrange
       const expectedArticlesTitle = 'Articles';
       const commentsPage = new CommentsPage(page);
-      const articlesPage = new ArticlesPage(page);
 
       // Act
       await commentsPage.goto();
-      await commentsPage.mainMenu.articlesButton.click();
+      const articlesPage = await commentsPage.mainMenu.clickArticlesButton();
       const title = await articlesPage.getTitle();
 
       // Assert
@@ -49,11 +46,10 @@ test.describe('Verify main menu buttons', () => {
       // Arrange
       const expectedHomePageTitle = 'GAD';
       const articlesPage = new ArticlesPage(page);
-      const homePage = new HomePage(page);
 
       // Act
       await articlesPage.goto();
-      await articlesPage.mainMenu.homePage.click();
+      const homePage = await articlesPage.mainMenu.clickArticlesButton();
       const title = await homePage.getTitle();
 
       // Assert
