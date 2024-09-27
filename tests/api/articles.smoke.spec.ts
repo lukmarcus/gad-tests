d7589d7dced5404ca5c05c8d91c48037617b5e84
Marek Szumny <lukmarcus@gmail.com>
2024-09-27 13:55:59 +0200

    chore: tags added for api test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,15 +1,19 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
-test.describe('Verify articles API endpoint', () => {
-  test('GET articles returns status code 200', async ({ request }) => {
-    // Arrange
-    const expectedStatusCode = 200;
-    const articlesUrl = '/api/articles';
+test.describe(
+  'Verify articles API endpoint',
+  { tag: ['@GAD-R08-01', '@api'] },
+  () => {
+    test('GET articles returns status code 200', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 200;
+      const articlesUrl = '/api/articles';
 
-    // Act
-    const response = await request.get(articlesUrl);
+      // Act
+      const response = await request.get(articlesUrl);
 
-    // Assert
-    expect(response.status()).toBe(expectedStatusCode);
-  });
-});
+      // Assert
+      expect(response.status()).toBe(expectedStatusCode);
+    });
+  },
+);
